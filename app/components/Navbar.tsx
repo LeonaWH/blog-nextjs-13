99a2a8831cd89aaa03563b8320e8876472c2aa47
LeonaWH <leonavarghesewh@gmail.com>
2023-07-10 09:08:53 +0530

    Initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import ThemeButton from "./ThemeButton";
+
+function Navbar(){
+    return (
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+                <div className="flex justify-between items-center w-full">
+                    <Link href="/">
+                        <h1 className="text-2xl font-medium">
+                            January <span className="text-teal-400">Blog</span>
+                        </h1>
+
+                    </Link>
+                    <ThemeButton />
+
+                </div>
+
+            </div>
+        </div>
+    )
+
+
+}
+export default Navbar
